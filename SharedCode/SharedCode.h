20a0436c6d831d95d37cca9f5ae6afcc2dc40741
jellybeanjohnny <mamerige@gmail.com>
2017-05-03 14:55:50 -0400

    Adds extension and shared framework

---
+++ SharedCode/SharedCode.h
@@ -0,0 +1,19 @@
+//
+//  SharedCode.h
+//  SharedCode
+//
+//  Created by Matt Amerige on 5/3/17.
+//  Copyright © 2017 Matt Amerige. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedCode.
+FOUNDATION_EXPORT double SharedCodeVersionNumber;
+
+//! Project version string for SharedCode.
+FOUNDATION_EXPORT const unsigned char SharedCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedCode/PublicHeader.h>
+
+
